f4e09340b1856f0d1e1ca8ca3f926892bd7d8664
Jhonatan Tipan <jhonatan.tipan@udla.edu.ec>
2023-12-04 21:52:31 -0500

    Pedir datos

---
+++ ArreglosTridimensionales.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int x, y, z;
+    
+    printf("Ingrese la dimensión x: ");
+    scanf("%d", &x);
+    printf("Ingrese la dimensión y: ");
+    scanf("%d", &y);
+    printf("Ingrese la dimensión z: ");
+    scanf("%d", &z);
+    
+    
+    return 0;
+}
