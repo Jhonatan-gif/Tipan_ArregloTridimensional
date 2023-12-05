d6b963021c35275a2c19dd5b5d63300b7dd3c48a
Jhonatan Tipan <jhonatan.tipan@udla.edu.ec>
2023-12-04 22:01:40 -0500

    Inicializar todas las matrices en CERO

--- ArreglosTridimensionales.c
+++ ArreglosTridimensionales.c
@@ -10,6 +10,18 @@ int main() {
     printf("Ingrese la dimensión z: ");
     scanf("%d", &z);
     
+    int arreglo[x][y][z];
+    
+    // Inicializar todas las matrices en cero
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++) {
+            for (int k = 0; k < z; k++) {
+                arreglo[i][j][k] = 0;
+            }
+        }
+    }
+    
+   
     
     return 0;
 }
